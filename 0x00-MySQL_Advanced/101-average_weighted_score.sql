43d29ecbc019adf070e303175731ef3f5fece698
Anotibills <anotibills@gmail.com>
2024-01-19 00:54:11 +0100

    tasks redone

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,20 +1,17 @@
--- A  SQL script that creates a stored procedure
+-- A SQL script that creates a stored procedure
 -- ComputeAverageWeightedScoreForUsers that computes
 -- and store the average weighted score for all students.
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
-BEGIN
-    UPDATE users AS U,
-        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
-        FROM users AS U 
-        JOIN corrections AS C ON U.id = C.user_id 
-        JOIN projects AS P ON C.project_id = P.id 
-        GROUP BY U.id)
-    AS WA
-    SET U.average_score = WA.w_avg 
-    WHERE U.id = WA.id;
-END
-$$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN `user_id` INT)
+BEGIN 
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END $$
 DELIMITER ;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@
 DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN `user_id` INT)
-BEGIN 
+BEGIN
     DECLARE avg_score FLOAT;
 
     SELECT AVG(score) INTO avg_score
@@ -14,7 +14,6 @@ BEGIN
         UPDATE users
         SET average_score = avg_score
         WHERE id = user_id;
-    ELSE
     END IF;
 END $$
 DELIMITER ;
