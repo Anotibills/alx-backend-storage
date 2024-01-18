3229e32adee3953a598a927bd5df1b708da1d00b
Anotibills <anotibills@gmail.com>
2024-01-19 00:42:28 +0100

    tasks corrected

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,14 @@
 -- A SQL script that creates a stored procedure ComputeAverageScoreForUser
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT)
-BEGIN
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN `user_id` INT)
+BEGIN 
     DECLARE avg_score FLOAT;
 
     SELECT AVG(score) INTO avg_score
-    FROM corrections AS C
-    WHERE C.user_id = user_id;
+    FROM corrections
+    WHERE corrections.user_id = user_id;
 
     IF avg_score IS NOT NULL THEN
         UPDATE users
@@ -17,6 +16,5 @@ BEGIN
         WHERE id = user_id;
     ELSE
     END IF;
-END;
-$$
+END $$
 DELIMITER ;
